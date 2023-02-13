fbf957f7fd67aea1f418e85abdc0d190886fd2c5
ZeroQLi <itsrandomboy77@gmail.com>
2023-02-13 22:28:07 +0400

    e

--- harmonoid-bin/lure.sh
+++ harmonoid-bin/lure.sh
@@ -1,1 +1,9 @@
-#TBD
+name='harmonoid-bin'
+version='0.3.5'
+release='1'
+desc='Plays & manages your music library. Looks beautiful & juicy. Playlists, visuals, synced lyrics, pitch shift, volume boost & more.'
+homepage='https://github.com/harmonoid/harmonoid'
+architectures=('amd64')
+license=('custom')
+provides('harmonoid')
+deps('mpv','xdg-user-dirs')
